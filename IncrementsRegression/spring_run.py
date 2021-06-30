4dbd7e06986bee61132c6dcad249793b31416ddf
Mathgo12 <sur123app@gmail.com>
2021-06-29 21:26:23 -0700

    Trained on all increments data

--- IncrementsRegression/spring_run.py
+++ IncrementsRegression/spring_run.py
@@ -14,7 +14,6 @@ def import_data(folderpath, data_name):
         if data_name in file:
             temp = pd.read_csv(folderpath + file)
             dfs.append(temp)
-
     total_df = pd.concat([df for df in dfs], axis=0)
     return total_df
 
@@ -40,7 +39,7 @@ def train_model(params, X_train, y_train, epochs=15):
     for epoch in range(epochs):
         rf.fit(X_train, y_train)
         rf_score = rf.score(X_train, y_train)
-        print(f"Epoch {epoch}; R squared: {rf_score}")
+        #print(f"Epoch {epoch}; R squared: {rf_score}")
         sum_rsquared += rf_score
 
     average_rsquared = sum_rsquared / epochs
@@ -59,24 +58,49 @@ def train_model(params, X_train, y_train, epochs=15):
 # min_samples_leaf
 
 folderpath = "..\\data\\"
-full_data = import_data(folderpath, "spring_age")
-X_train, X_test, y_train, y_test = data_preprocessing(full_data, ['stock'], ['year'])
+#identifier = "spring_age"
 
-random_forest, rsquared = build_model(
-    {
-        "n_estimators":100,
-        "oob_score":True,
-        "min_impurity_decrease":0.1
-    },
-    X_train,
-    y_train,
-    epochs=10
-)
+data_identifiers = [
+"spring_age",
+"spring_age_2_3",
+"spring_age_3_4",
+"spring_age_4_5",
+"GOA_fall_age",
+"GOA_fall_age_1_2",
+"GOA_fall_age_2_3",
+"GOA_fall_age_3_4",
+"GOA_fall_age_4_5",
+"increments_NCC",
+"NCC_fall_age_1_2",
+"NCC_fall_age_2_3",
+"NCC_fall_age_3_4",
+"NCC_fall_age_4_5",
+"fall_age",
+"fall_age_1_2",
+"fall_age_2_3",
+"fall_age_3_4",
+"fall_age_4_5",
+"2_3",
+"3_4",
+"4_5"
 
+]
 
+for identifier in data_identifiers:
+    full_data = import_data(folderpath, identifier)
+    X_train, X_test, y_train, y_test = data_preprocessing(full_data, ['stock'], ['year'])
 
+    random_forest, rsquared = train_model(
+        {
+            "n_estimators":100,
+            "oob_score":True,
+            "min_impurity_decrease":0.1
+        },
+        X_train,
+        y_train,
+        epochs=10
+    )
 
-
-
+    print(f"Dataset: {identifier}; Average R-Squared {rsquared}")
 
 
