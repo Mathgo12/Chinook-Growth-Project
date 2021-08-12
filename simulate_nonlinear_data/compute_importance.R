0e45449ca967b4b992a1acd6a3268b1cbed581e4
Jack Buckner <Jhbuckner@ucdavis.edu>
2021-08-11 21:03:51 -0400

    added variable importnace to simualted data

---
+++ simulate_nonlinear_data/compute_importance.R
@@ -0,0 +1,28 @@
+library(dplyr)
+
+setwd("~/github/Chinook-Growth-Project")
+pars <- read.csv("simulate_nonlinear_data/test/par.csv")
+
+compute_importance <- function(j){
+  dat <- read.csv(paste("simulate_nonlinear_data/data_set_",j,"/par.csv",sep = ""))
+  print(dat)
+  impvals <- rep(0,20)
+  
+  for(i in 1:nrow(dat)){
+    if(is.na(dat$V2[i])){
+      impvals[dat$V1[i]] <- impvals[dat$V1[i]] + dat$effect_size[i]^2
+    }else{
+      impvals[dat$V1[i]] <- impvals[dat$V1[i]] + abs(dat$effect_size[i])
+      impvals[dat$V2[i]] <- impvals[dat$V2[i]] + abs(dat$effect_size[i])
+    }
+  }
+  
+  impvals <- impvals[impvals != 0]
+  print(impvals)
+  dat <- data.frame(V = 1:length(impvals), Imp = impvals)
+  print(dat)
+  write.csv(dat,paste("simulate_nonlinear_data/data_set_",j,"/Imp.csv",sep = ""))
+}
+
+#compute_importance(3)
+parallel::mclapply(1:100,compute_importance, mc.cores = 10)
